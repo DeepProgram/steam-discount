5eb471f07c5057b46e23e4c471290e4437fccad9
DeepProgram <110674579+DeepProgram@users.noreply.github.com>
2022-08-07 00:38:20 +0600

    Update sendapirequest.py

--- sendapirequest.py
+++ sendapirequest.py
@@ -2,9 +2,20 @@ import time
 from datetime import datetime,timedelta
 import json
 import requests
+"""
 data = {
   "searchkey":"discount",
-  "value":"0"
+  "value":"80"
+  }
+  
+ data = {
+  "searchkey":"price",
+  "value":"20.00"
+  }
+"""
+data = {
+  "searchkey":"date",
+  "value":"2022-08-06"
   }
 response = requests.get("http://127.0.0.1:5000/getinfo",json=data)
 print(response.text)
